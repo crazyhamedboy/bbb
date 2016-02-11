2f7b1d0a932d4f828fc34328148072accf71787c
crazyhamedboy <hamedcrazyboy@gmail.com>
2016-02-11 10:46:34 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,6 +219,9 @@ function create_config( )
     "arabic_lock",
     "set",
     "get",
+    "sms",
+    "text",
+    "antilink",
     "broadcast",
     "download_media",
     "invite",
@@ -226,7 +229,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {126049263,172178919(172178919)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
